276dbbe5cc92775f18c68868dd804a3c23b89c5a
Cristiano Pureza <50499741+cristianopureza@users.noreply.github.com>
2024-08-13 03:01:02 -0300

    Update upgrade.sh

--- upgrade.sh
+++ upgrade.sh
@@ -1,2 +1,2 @@
 #upgrade zabbix version 6.4 on rhel 8
-rpm -Uvh https://repo.zabbix.com/zabbix/6.4/rhel/8/x86_64/zabbix-release-latest.el8.noarch.rpm && dnf update zabbix* -y && systemctl restart zabbix-proxy
+rpm -Uvh https://repo.zabbix.com/zabbix/6.4/rhel/8/x86_64/zabbix-release-latest.el8.noarch.rpm && dnf update zabbix* -y && systemctl restart zabbix-proxy && rm -Rf zbx-upgrade* && exit
